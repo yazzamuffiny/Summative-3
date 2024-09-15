0d2b0c04a4616f00f0cd5ef7c531a44240ed4c2a
caitlinanderson099 <caitlin.anderson099@gmail.com>
2024-09-16 11:43:30 +1200

    created the hooks and context in the seller frontend and created the folder for nav bar, copied that into the buyer frontend, just need to change the logout button to login and sign up buttons

--- buyer-frontend/src/components/nav-bar/Nav.jsx
+++ buyer-frontend/src/components/nav-bar/Nav.jsx
@@ -1,9 +1,30 @@
-import React from 'react'
+import { useLogout } from '../../hooks/useLogout';
+import { useAuthContext } from '../../hooks/useAuthContext';
+import './nav.scss'
 
 const Nav = () => {
+
+  const {logout} = useLogout();
+  const {user} = useAuthContext();
+
+  const handleLogout = () => {
+    logout();
+  }
+
   return (
-    <div>
-      Nav
+    <div className='navbar'>
+
+      <div className='logo'>
+        <img src="src/images/logo-no-bg.png" alt="website logo"/>
+      </div>
+
+      {/* change this to the login signup buttons */}
+      <div className='logged-user'>
+        {user && <div className="userInt">
+          <span>{user.email}</span>
+          <button onClick={handleLogout}> Logout </button>
+          </div>}
+      </div>
     </div>
   )
 }
--- seller-frontend/src/main.jsx
+++ seller-frontend/src/main.jsx
@@ -1,10 +1,12 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import React from "react"
+import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import {AuthContextProvider} from './context/AuthContext.jsx'
 
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <AuthContextProvider>
+        <App/>
+    </AuthContextProvider>
+  </React.StrictMode>,
 )
