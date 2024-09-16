e66ff1fb7d9624c49c9b01ab0aec83601d574f0a
Yasmin Feisst <yasminfeisst99@gmail.com>
2024-09-16 12:24:05 +1200

    Buyers filter works (probably)
    basic grid display, need better data
    
    needs propper css later

---
+++ buyer-frontend/src/pages/listingdetails/ListingDetails.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const ListingDetails = ({listing}) => {
+  return (
+    <div className='listing-card'>
+        <p>{listing.breed}</p>
+      
+    </div>
+  )
+}
+
+export default ListingDetails
